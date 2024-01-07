2a61efb2e012be49470f3ae3268b52c386dc6f6b
MathisNcl <mathis.nicoli@gmail.com>
2024-01-07 19:48:13 +0100

    :construction: WIP
    Signed-off-by: MathisNcl <mathis.nicoli@gmail.com>

--- development/test_yolov8.py
+++ development/test_yolov8.py
@@ -1,26 +1,38 @@
 """Script test"""
-# from ultralytics import YOLO
+# from hooptrack.basketball.inference_pipeline import InferencePipelineBasketBall
+# from hooptrack.schemas.config import BasketballDetectionConfig
 
-# stream = True to save lot of time
-# model = YOLO("models/best_nano_22h21.pt")
-# res = model.predict(
-#     "data/VID1.mp4",
-#     save=True,
-#     conf=0.25,
-#     max_det=4,
-#     save_txt=True,
-# )
-# model.predict(source="0", show=True)
-# model.export(format="onnx")
+# from hooptrack.basketball.visualiser import Visualiser
+# from PIL import Image
+# from cProfile import Profile
+# from pstats import SortKey, Stats
+
+
+# with Profile() as profile:
+#     inference_pipeline = InferencePipelineBasketBall(
+#         config=BasketballDetectionConfig(
+#             model="/Users/mathisnicoli/Desktop/PROJETS/HoopTrack/models/best_nano_22h21.onnx"
+#         )
+#     )
+#     res = inference_pipeline.run("/Users/mathisnicoli/Desktop/PROJETS/HoopTrack/data/img/capture1.png")
+#     (Stats(profile).strip_dirs().sort_stats(SortKey.TIME).print_stats())
+# visualiser = Visualiser()
+
+# im = visualiser.plot(res, show_id=True)
+
+# Image.fromarray(im).show()
 
 
 from hooptrack.basketball.inference_pipeline import InferencePipelineBasketBall
 from hooptrack.schemas.config import BasketballDetectionConfig
 
 inference_pipeline = InferencePipelineBasketBall(
-    config=BasketballDetectionConfig(model="/Users/mathisnicoli/Desktop/PROJETS/HoopTrack/models/best_nano_22h21.onnx")
+    config=BasketballDetectionConfig(
+        model="/Users/mathisnicoli/Desktop/PROJETS/HoopTrack/models/best_nano_22h21.onnx", frame_processed_every=6
+    )
 )
 
-res = inference_pipeline.run("/Users/mathisnicoli/Desktop/PROJETS/HoopTrack/data/img/capture1.png")
+inference_pipeline.live_streaming(source="/Users/mathisnicoli/Downloads/IMG_6226.MOV", save=True, show=False)
 
-print(res)
+
+inference_pipeline.live_streaming(source=0, save=True, show=False)
